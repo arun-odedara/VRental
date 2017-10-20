18ad8d4b0c98724b986ad7267f64fef3d049fa98
arun34 <odedara.arun34@gmail.com>
2017-10-19 23:53:42 -0500

    Initial Commit
    WEb Development with C#/.NET MVC

---
+++ VRental/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VRental.Models;
+using VRental.ViewModels;
+
+namespace VRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Arun Kumar" },
+                new Customer { Name = "Raj Mehra" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ VRental/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VRental.Models;
+
+namespace VRental.ViewModels
+{
+    public class CustomerListViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
